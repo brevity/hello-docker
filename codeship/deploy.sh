bf23da4039ffbec4e94d7c501b63a39bc71dce0c
Andrew Toelle <andrew.toelle@gmail.com>
2015-03-21 18:02:41 -0500

    add codeship scripts

---
+++ codeship/deploy.sh
@@ -0,0 +1,10 @@
+cd ..
+git clone git@bitbucket.org:ajtoelle/hello-docker-creds.git
+source /home/rof/clone/hello-docker-creds/aws_credentials
+cd ansible
+pip install boto
+pip install ansible
+which python
+echo 'Maybe check if an EC2 instance exists first?'
+env | grep AWS
+ansible-playbook -i hosts provision.yaml
